f6c462c04dc22f3214675ce6f92a5c4a7620a2e6
AlishaMeier <alisha.meyer@gmail.com>
2025-09-09 23:24:28 +0500

    Completed middle lvl

--- test_demoqa_input_form.py
+++ test_demoqa_input_form.py
@@ -1,35 +1,34 @@
-from selene.support.shared import browser
-from selene import have
-import os
+from pages.registration_page import RegistrationPage
 
+def test_fill_form():
+    registration_page = RegistrationPage()
+    registration_page.open()
+    (
+        registration_page
+        .fill_first_name('Alisha')
+        .fill_last_name('Meier')
+        .fill_email('alisha.meyer@gmail.com')
+        .set_gender("Female")
+        .fill_phone_number('7078083333')
+        .fill_birthday('November', '1995', '03')
+        .set_subject_by_enter('Computer Science')
+        .set_hobby("Music")
+        .upload_picture('test_check.txt')
+        .fill_current_address('ул.Назарбаева, 69')
+        .choose_location('NCR', 'Delhi')
+        .submit_form()
+    )
 
-def test_demo_tools(window_configurate_chrome):
-    browser.element('#firstName').type('Alisha')
-    browser.element('#lastName').type('Meier')
-    browser.element('#userEmail').type('alisha.meyer@gmail.com')
-    browser.element('#gender-radio-2').double_click()
-    browser.element('#userNumber').set_value('9969878866')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click().element('[value="10"]').click()
-    browser.element('.react-datepicker__year-select').click().element('[value="1995"]').click()
-    browser.element('[aria-label="Choose Friday, November 3rd, 1995"]').click()
-    browser.element('#subjectsInput').type('Computer Science').press_enter()
-    browser.element('[for="hobbies-checkbox-2"]').click()
-    browser.element('[for="hobbies-checkbox-3"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('../image/wolf_tester.jpeg'))
-    browser.element('#currentAddress').type('Kaliningrad')
-    browser.element('#react-select-3-input').type('NCR').press_enter()
-    browser.element('#react-select-4-input').type('Gurgaon').press_enter()
-    browser.element('#submit').scroll_to().press_enter()
-    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-    browser.all('.modal-body td+td').should(have.texts(
-        'Alisha Meier',
-        'alisha.meyer@gmail.com',
-        'Female',
-        '9969878866',
-        '03 November,1995',
-        'Computer Science',
-        'Reading, Music',
-        'wolf_tester.jpeg',
-        'Kaliningrad',
-        'NCR Gurgaon'))
+    registration_page.should_have_registered_user_with(
+            'Alisha Meier',
+            'alisha.meyer@gmail.com',
+            'Female',
+            '7078083333',
+            '03 November,1995',
+            'Computer Science',
+            'Music',
+            'test_check.txt',
+            'ул.Назарбаева, 69',
+            'NCR Delhi'
+                                                 )
+    print('Success')
